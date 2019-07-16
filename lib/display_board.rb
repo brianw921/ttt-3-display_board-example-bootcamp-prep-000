fa4c78e360709ab62794a770cb51e7779b0685c4
Brian Wong <bwong92@gmail.com>
2019-07-16 13:55:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 def display_board
   output = ["   ", "   |   ", "   ", "   |   ", "   "]
   print output
-  # puts "-----------"
-  # print output
-  # puts "-----------"
-  # print output
+  puts "-----------"
+  print output
+  puts "-----------"
+  print output
 end
 
 
